cf7f8c11f6c2cbaf55861dff5f65362d1f7f38ca
flyperson <1270629943@qq.com>
2017-07-19 10:18:15 +0800

    2017-7-19

--- js/service.js
+++ js/service.js
@@ -4,7 +4,7 @@
 //url='http://202.105.179.167:8080/zjadmin/zjejsgs/';//服务器url
 //url='http://192.168.0.11:8081/zjejsgs/';
 //url='http://192.168.123.1:8081/zjejsgs/';
-url='http://192.168.0.14:8081/zjejsgs/';
+url='http://192.168.0.14:8081/zjejsgs-github/';
 var gcmcxx='http://192.168.0.11:8081/hxadmin2/ProjectPlus/';
 var servername='root';
 var serverpassword='123456';
